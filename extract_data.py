866107475fd3db8bb27d8b9c6585f64b85ed2a48
czliou <48034215+czliou@users.noreply.github.com>
2021-02-26 13:48:30 -0600

    homo/lumo dict

--- extract_data.py
+++ extract_data.py
@@ -35,4 +35,31 @@ smiles_data = dict()
 for i in range(len(data['key_value_pairs'])):
   smiles_data[i] = data['key_value_pairs'][0]['Smiles']
 
+# Initial work to modify the g4mp2_data.json file
+# with open('g4mp2_data.json', 'r') as infile:
+#   data = infile.read()
+
+# data = data.replace("\n", ",\n")
+# with open('g4mp2_data_modified.json', 'w') as outfile:
+#   outfile.write("{\n\"data\":[\n")
+#   outfile.write(data)
+#   outfile.write("]}")
+
+#open json file
+with open('g4mp2_data_modified.json', 'r') as infile:
+  data = json.load(infile)
+  data = data['data']
+
+#dictionary for SMILES string
+homo_lumo_data = dict()
+for i in range(len(data)):
+  homo_lumo_data[i] = dict()
+  homo_lumo_data[i]['homo'] = data[i]['homo']
+  homo_lumo_data[i]['lumo'] = data[i]['lumo']
+  homo_lumo_data[i]['index'] = data[i]['index']
+
+for i in range(10):
+  print(homo_lumo_data[i])
+
+
 sql_connect.close()
